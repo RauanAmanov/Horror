39b8b0a9c1647cb224c15f1712c240b590103a97
RauanAmanov <rauan1405@mail.ru>
2020-10-09 04:36:59 +0600

    cube runner with Music

---
+++ cube runner/Assets/Scripts/button.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class button : MonoBehaviour {
+  // Start is called before the first frame update
+  public bool _soundOn = true;
+  private void Awake() {
+  }
+  public void OnButtonClick() {
+    if (_soundOn) {
+      FindObjectOfType<Music>().StopMusic();
+      _soundOn = false;
+    } else {
+      FindObjectOfType<Music>().PlayMusic();
+      _soundOn = true;
+    }
+  }
+
+  // Update is called once per frame
+
+}
---
+++ cube runner/Assets/Scripts/gameButton.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gameButton : MonoBehaviour {
+  bool soundOn;
+  void Awake() {
+    soundOn = FindObjectOfType<Music>()._audioSource.isPlaying ? true : false;
+  }
+  // Start is called before the first frame update
+  public void OnClick() {
+    if (soundOn) {
+      FindObjectOfType<Music>().StopMusic();
+      soundOn = false;
+    } else {
+      FindObjectOfType<Music>().PlayMusic();
+      soundOn = true;
+    }
+  }
+
+  // Update is called once per frame
+
+}
